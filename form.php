29ece17aab6bfe2802339f014f90d481e42e5216
Hsheikh20 <63617385+Hsheikh20@users.noreply.github.com>
2020-06-12 09:05:49 -0700

    Update form.php

--- form.php
+++ form.php
@@ -1,9 +1,12 @@
 <?php
-if()}else{// show data
+if(isset($_GET["FirstName"])){// show data
+  echo $_GET["FirstName"];
 
 }else{//show form
   echo ' 
   <form action="">
+  <p>First Name: <input type= "text" name="FirstName" /></p> 
+  <p>input type="submit" /></p>
   
   
   </form>
